151e03b3b251a5752ff07e8383f9538dc444a671
Wessel Scholtz <wessels@bbd.co.za>
2022-02-18 23:20:42 +0200

    Added Cart SQL

---
+++ SQLScripts/Tables/Cart.sql
@@ -0,0 +1,16 @@
+-- Create a new table called 'Cart' in schema 'LTECdb'
+USE [LTECdb]
+GO
+-- Create the table
+CREATE TABLE [dbo].[Cart]
+(
+    OrderID [INT] NOT NULL,          -- primary / foreign key column
+    CakeID [INT] NOT NULL,           -- primary / foreign key column
+    Quantity [TINYINT] NOT NULL,
+    ItemPrice [SMALLMONEY] NULL 
+);
+GO
+ALTER TABLE [dbo].[Cart] WITH ADD CONSTRAINT [pk_Cart_OrderID] PRIMARY KEY CLUSTERED ([OrderID],[CakeID]);
+ALTER TABLE [dbo].[Cart] WITH ADD CONSTRAINT [fk_Cart_OrderID] FOREIGN KEY([OrderID]) REFERENCES [dbo].[Order] ([OrderID]);
+ALTER TABLE [dbo].[Cart] WITH ADD CONSTRAINT [fk_Cart_CakeID]  FOREIGN KEY([CakeID])  REFERENCES [dbo].[Cake] ([CakeID]);
+GO
