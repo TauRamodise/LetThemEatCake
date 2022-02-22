379138a370af7d77db75912260a588d5efbbf10e
BawinileM <47977371+BawinileM@users.noreply.github.com>
2022-02-22 14:01:53 +0200

    Add files via upload
    added updated customer and the location table

---
+++ CreateCustomerTable.sql
@@ -0,0 +1,13 @@
+-- Create a new table called 'Customer' in schema 'LTECdb'
+USE [LTECdb]
+GO
+-- Create the table
+CREATE TABLE [dbo].[Customer]
+(
+    [CustomerID] [INT] NOT NULL,                        -- primary key column
+    [FullName] [VARCHAR](250) NOT NULL,   
+    [PhoneNumber] [VARCHAR](31) NOT NULL,        
+    [Email] [VARCHAR](250) NOT NULL,
+    [DeliveryLocationID] [VARCHAR](250) NULL            -- foreign key column
+    [FavouriteCakeID] [INT] NULL                        -- foreign key column
+);
