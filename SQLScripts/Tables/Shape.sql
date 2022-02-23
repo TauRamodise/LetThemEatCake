fa8688f688fed74e695e8ac7ec46f659f3b5de3f
Waseem Ahmed <waseema@bbd.co.za>
2022-02-23 16:55:39 +0200

    refactor shape table

--- SQLScripts/Tables/Shape.sql
+++ SQLScripts/Tables/Shape.sql
@@ -3,9 +3,40 @@ GO
 
 CREATE TABLE [dbo].[Shape](
 	[ShapeID] [int] IDENTITY(1,1) NOT NULL,
-    [ShapeDescription] [varchar](1000) NOT NULL,
+    [ShapeName] [varchar](250) NOT NULL,
     [ShapePrice] [smallmoney] NOT NULL,
     CONSTRAINT PK_Shape PRIMARY KEY (ShapeID)
 );
 
 GO
+
+CREATE PROCEDURE [dbo].[InsertUpdateDeleteShape]
+@ShapeName [varchar](250),
+@ShapePrice [smallmoney],
+@StatementType [varchar](6)
+AS
+  BEGIN
+      IF @StatementType = 'Insert'
+        BEGIN
+            INSERT INTO Shape
+            VALUES  ( 
+                        @ShapeName,
+                        @ShapePrice
+                    )
+        END
+
+    --   IF @StatementType = 'Update'
+    --     BEGIN
+    --         UPDATE Shape
+    --         SET    ShapeName = @first_name,
+    --                ShapePrice = @last_name,
+    --                salary = @salary,
+    --                city = @city
+    --         WHERE  id = @id
+    --     END
+    --   ELSE IF @StatementType = 'Delete'
+    --     BEGIN
+    --         DELETE FROM Shape
+    --         WHERE  id = @id
+    --     END
+  END
