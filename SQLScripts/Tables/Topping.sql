6774388f5c55cecef6aedd6651e7291015627c0e
Waseem Ahmed <waseema@bbd.co.za>
2022-02-23 16:56:30 +0200

    refactor topping table

--- SQLScripts/Tables/Topping.sql
+++ SQLScripts/Tables/Topping.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE TABLE [dbo].[Topping](
 	[ToppingID] [int] IDENTITY(1,1) NOT NULL,
-    [ToppingDescription] [varchar](1000) NOT NULL,
+    [ToppingName] [varchar](250) NOT NULL,
     [ToppingPrice] [smallmoney] NOT NULL,
     CONSTRAINT PK_Topping PRIMARY KEY (ToppingID)
 );
