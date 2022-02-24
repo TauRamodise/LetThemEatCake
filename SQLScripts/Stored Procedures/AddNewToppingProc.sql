5050748fe632da3bf92c18aa6a37225815b574d6
Waseem Ahmed <waseema@bbd.co.za>
2022-02-24 16:37:35 +0200

    add stored procedure for adding topping

---
+++ SQLScripts/Stored Procedures/AddNewToppingProc.sql
@@ -0,0 +1,16 @@
+USE LetThemEatCakeDB;
+GO
+
+CREATE PROCEDURE [dbo].[InsertNewTopping]
+@ToppingName [varchar](250),
+@ToppingPrice [smallmoney],
+AS
+  BEGIN
+    INSERT INTO Topping
+    VALUES  ( 
+                @ToppingName,
+                @ToppingPrice
+            )
+  END
+ 
+--EXEC dbo.InsertNewTopping @ToppingName = 'NewTopping', @ToppingPrice = 100, @TableName = 'Topping';
