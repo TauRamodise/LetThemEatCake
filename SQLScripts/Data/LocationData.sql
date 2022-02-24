893b95269ef911b8bd0c463e5846d562790eb310
Waseem Ahmed <waseema@bbd.co.za>
2022-02-24 17:14:18 +0200

    add location data

--- SQLScripts/Data/LocationData.sql
+++ SQLScripts/Data/LocationData.sql
@@ -1,23 +1,27 @@
-USE LetThemEatCakeDB
-GO
-INSERT INTO [dbo].[Location] (Coordinates)  
-VALUES (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-26.270187, 27.885902, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 )),
-	   (geography::Point(-25.705357, 28.262071, 4326 ));
-GO
+INSERT INTO [dbo].[Location](                   
+    [Coordinates]
+)
+[16:45] Wessel Scholtz
+VALUES (
+    geography::Point(-26.207032, 27.918984, 4326 ),
+    geography::Point(-26.111193, 28.102838, 4326 ),
+    geography::Point(-26.111193, 28.102838, 4326 ),
+    geography::Point(-26.111193, 28.102838, 4326 ),
+    geography::Point(-26.111193, 28.102838, 4326 ),
+    geography::Point(-25.898117, 28.154793, 4326 ),
+    geography::Point(-25.854057, 28.149371, 4326 ),
+    geography::Point(-25.858179, 28.149488, 4326 ),
+    geography::Point(-25.865882, 28.141127, 4326 ),
+    geography::Point(-25.877333, 28.128944, 4326 ),
+    geography::Point(-25.968458, 28.204642, 4326 ),
+    geography::Point(-25.983869, 28.172364, 4326 ),
+    geography::Point(-25.963305, 28.147706, 4326 ),
+    geography::Point(-25.947037, 28.057013, 4326 ),
+    geography::Point(-26.118017, 28.058268, 4326 ),
+    geography::Point(-26.139041, 28.041916, 4326 ),
+    geography::Point(-26.130274, 28.023841, 4326 ),
+    geography::Point(-26.127750, 28.018444, 4326 ),
+    geography::Point(-26.124629, 28.021330, 4326 )
+    );
+GO    
+           
