81a1d3393d387b1ec939ce8496c22d5e5a1b7e1b
Dimpho <DimphoR@bayportfinance.com>
2022-02-24 09:27:54 +0200

    Add icing and base table scripts

---
+++ SQLScripts/Tables/Bases.sql
@@ -0,0 +1,18 @@
+/*
+    Bases - defined as a cake base flavour
+    Columns - BaseID  Primary Key  Identity(1,1)  Int  Not Null
+            - BaseName  varchar(250)  Not Null
+            - BasePrice  smallMoney  Not Null
+*/
+
+USE LetThemEatCakeDB;
+GO
+
+CREATE TABLE [dbo].[Base] (
+    [BaseID] [int] IDENTITY(1,1) NOT NULL,
+    [BaseName] [varchar(250)] NOT NULL,
+    [BasePrice] [smallMoney] NOT NULL,
+    CONSTRAINT PK_Base PRIMARY KEY (BaseID)
+)
+
+GO
---
+++ SQLScripts/Tables/Icing.sql
@@ -0,0 +1,18 @@
+/*
+    Icing - defined as a cake Icing flavour
+    Columns - IcingID  Primary Key  Int  Not Null
+            - IcingName  varchar(250)  Not Null
+            - IcingPrice  smallMoney  Not Null
+*/
+
+USE LetThemEatCakeDB;
+GO
+
+CREATE TABLE [dbo].[Icing] (
+    [IcingID] [int] IDENTITY(1,1) NOT NULL,
+    [IcingName] [varchar(250)] NOT NULL,
+    [IcingPrice] [smallMoney] NOT NULL,
+    CONSTRAINT PK_Icing PRIMARY KEY (IcingID)
+)
+
+GO
