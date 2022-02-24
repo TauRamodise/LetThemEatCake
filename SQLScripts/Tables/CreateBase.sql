44736c4e3442ecae4828fb99e5359af1271f9571
Waseem Ahmed <48731286+wasahmed@users.noreply.github.com>
2022-02-24 10:33:34 +0200

    fix base name data type

--- SQLScripts/Tables/CreateBase.sql
+++ SQLScripts/Tables/CreateBase.sql
@@ -10,7 +10,7 @@ GO
 
 CREATE TABLE [dbo].[Base] (
     [BaseID] [int] IDENTITY(1,1) NOT NULL,
-    [BaseName] [varchar(250)] NOT NULL,
+    [BaseName] [varchar](250) NOT NULL,
     [BasePrice] [smallMoney] NOT NULL,
     CONSTRAINT PK_Base PRIMARY KEY (BaseID)
 )
