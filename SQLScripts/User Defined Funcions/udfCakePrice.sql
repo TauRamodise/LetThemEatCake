7cef259ff13f4201ba130950bf0e41a865733a2c
Wessel Scholtz <wessels@bbd.co.za>
2022-02-24 18:10:34 +0200

    udfCakePrice works now

--- SQLScripts/User Defined Funcions/udfCakePrice.sql
+++ SQLScripts/User Defined Funcions/udfCakePrice.sql
@@ -1,19 +1,22 @@
 USE [LetThemEatCakeDB]
 GO
-DROP FUNCTION IF EXISTS [udfCakePrice];   
-CREATE FUNCTION [dbo].[udfCakePrice](@CakeID int)  
+DROP FUNCTION IF EXISTS [udfCakePrice];
+GO
+CREATE FUNCTION [dbo].[udfCakePrice](@CakeID int)
 RETURNS SMALLMONEY
-AS 
-BEGIN  
-    DECLARE @ret int;  
-    SELECT @ret = SUM([b].[BasePrice], [i].[IcingPrice], [t].[ToppingPrice], [s].[ShapePrice])   
+AS
+BEGIN
+    DECLARE @ret int;
+    SELECT @ret = ([b].[BasePrice]  + [i].[IcingPrice] + [t].[ToppingPrice] + [s].[ShapePrice])
     FROM [dbo].[Cake] AS c
-    JOIN [dbo].[Base] AS b ON [c].[BaseID] = [b].[BaseID]  
-    JOIN [dbo].[Icing] AS b ON [c].[IcingID] = [i].[IcingID]  
-    JOIN [dbo].[Topping] AS b ON [c].[ToppingID] = [t].[ToppingID]  
-    JOIN [dbo].[Shape] AS b ON [c].[ShapeID] = [s].[ShapeID]  
- 
-    IF (@ret IS NULL)   
-        SET @ret = 0;  
-    RETURN @ret;  
+    INNER JOIN [dbo].[Base] AS b ON [c].[BaseID] = [b].[BaseID]
+    INNER JOIN [dbo].[Icing] AS i ON [c].[IcingID] = [i].[IcingID]
+    INNER JOIN [dbo].[Topping] AS t ON [c].[ToppingID] = [t].[ToppingID]
+    INNER JOIN [dbo].[Shape] AS s ON [c].[ShapeID] = [s].[ShapeID]
+	WHERE
+		c.CakeID = @CakeID;
+
+    IF (@ret IS NULL)
+        SET @ret = 0;
+    RETURN @ret;
 END;
