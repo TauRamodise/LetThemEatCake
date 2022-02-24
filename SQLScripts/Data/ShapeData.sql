ad7420683032b56f82af49c744567f58a9d5e5b5
Waseem Ahmed <waseema@bbd.co.za>
2022-02-24 13:33:57 +0200

    add dummy data for shape and toping

---
+++ SQLScripts/Data/ShapeData.sql
@@ -0,0 +1,10 @@
+USE [LetThemEatCakeDB]
+GO
+
+INSERT INTO Shape VALUES ('Circle', 50)
+INSERT INTO Shape VALUES ('Square', 40)
+INSERT INTO Shape VALUES ('Rectangle', 42)
+INSERT INTO Shape VALUES ('Triangle', 55)
+INSERT INTO Shape VALUES ('Diamond', 60)
+INSERT INTO Shape VALUES ('Oval', 62)
+GO
