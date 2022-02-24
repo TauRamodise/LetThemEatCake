e0ee1b6d0a69d220c39590c072ef3079040472bd
Wessel Scholtz <wessels@bbd.co.za>
2022-02-24 08:48:00 +0200

    Consolidated CreateDataBase Script

--- SQLScripts/CreateDatabase.sql
+++ SQLScripts/CreateDatabase.sql
@@ -1,11 +1,17 @@
+-- Create a new database called 'LetThemEatCakeDB'
 USE master;
-IF EXISTS(SELECT * FROM sys.databases WHERE name='LetThemEatCakeDB')
-DROP DATABASE LetThemEatCakeDB
+IF EXISTS (SELECT * FROM sys.databases WHERE [name] = N'LetThemEatCakeDB')    -- see https://blog.greglow.com/2021/06/24/reliably-dropping-a-sql-server-database-if-it-exists/
+BEGIN
+    ALTER DATABASE [LetThemEatCakeDB] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;  -- as per https://stackoverflow.com/questions/33890085/how-to-drop-a-database-when-its-currently-in-use
+    DROP DATABASE [LetThemEatCakeDB];
+END;
 GO
 
 
 CREATE DATABASE LetThemEatCakeDB;
+COLLATE Latin1_General_CI_AS        -- see https://docs.microsoft.com/en-us/sql/relational-databases/collations/collation-and-unicode-support?view=sql-server-ver15#Server-level-collations
 GO
+
 
 USE LetThemEatCakeDB;
 GO
