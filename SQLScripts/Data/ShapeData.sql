e4ab66d14680432092e95ce51972d42c9fe7e8d6
MikaylaBBD <mikaylan@bbd.co.za>
2022-02-24 14:16:33 +0200

    Cake Foreign keys

--- SQLScripts/Data/ShapeData.sql
+++ SQLScripts/Data/ShapeData.sql
@@ -1,11 +1,14 @@
 USE [LetThemEatCakeDB]
 GO
 
-INSERT INTO Shape VALUES ('Circle', 50),
-('Square', 40),
-('Rectangle', 42),
-('Triangle', 55,
-('Diamond', 60),
-('Oval', 62),
-('Doughnut', 60)
+INSERT INTO [dbo].[Shape] (
+    [ShapeName], [ShapePrice]
+) 
+VALUES ('Circle', 50),
+    ('Square', 40),
+    ('Rectangle', 42),
+    ('Triangle', 55,
+    ('Diamond', 60),
+    ('Oval', 62),
+    ('Doughnut', 60);
 GO
