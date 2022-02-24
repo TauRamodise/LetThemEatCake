2d805b6837fd597965962c31f75f09695934e20c
Wessel Scholtz <wessels@bbd.co.za>
2022-02-24 18:22:27 +0200

    Added udfOrderprice

---
+++ SQLScripts/User Defined Funcions/udfOrderPrice.sql
@@ -0,0 +1,19 @@
+USE [LetThemEatCakeDB]
+GO
+DROP FUNCTION IF EXISTS [udfOrderPrice];
+GO
+CREATE FUNCTION [dbo].[udfOrderPrice](@OrderID int)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @ret int;
+    SELECT @ret = SUM([c].[ItemPrice])
+    FROM [dbo].[Cart] AS c
+	WHERE
+		c.OrderID = @OrderID;
+
+    IF (@ret IS NULL)
+        SET @ret = 0;
+    RETURN @ret;
+END;
+
