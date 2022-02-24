749bb25d43bd4cca4db6b0288c48337485ee110a
MikaylaBBD <mikaylan@bbd.co.za>
2022-02-24 14:11:25 +0200

    Updated format

--- SQLScripts/Data/CakeData.sql
+++ SQLScripts/Data/CakeData.sql
@@ -6,17 +6,17 @@ INSERT INTO [dbo].[Cake] (
 	[ToppingID],
 	[ShapeID]
 )
-VALUES ('Home-made Chocolate Cake', 'Traditional, rectangular home-made chocolate cake topped with chocolate buttercream icing and chocolate shavings.', 1, 13, 6, 3),
-	('Traditional Birthday Cake', 'Round vanilla cake with vanilla buttercream icing and multicoloured sprinkles.', 9, 12, 7, 1),
-	('Blue Birthday Cake', 'Round blueberry cake with blueberry flavoured buttercream icing, topped with home-made blueberry jam.', 13, 14, 8, 1),
-	('Rainbow Cake', 'Round cake made with rainbow coloured swirls. This cake is topped with vanilla buttercream icing and rainbow sprinkles.', 17, 14, 7, 1),
+VALUES ('Home-made Chocolate Cake', 'Traditional, rectangular home-made chocolate cake topped with chocolate buttercream icing and chocolate shavings.', 1, 13, 6, 1),
+	('Traditional Birthday Cake', 'Round vanilla cake with vanilla buttercream icing and multicoloured sprinkles.', 9, 12, 7, 2),
+	('Blue Birthday Cake', 'Round blueberry cake with blueberry flavoured buttercream icing, topped with home-made blueberry jam.', 13, 14, 8, 2),
+	('Rainbow Cake', 'Round cake made with rainbow coloured swirls. This cake is topped with vanilla buttercream icing and rainbow sprinkles.', 17, 14, 7, 2),
 	('Square Rainbow Cake', 'Square cake made with rainbow coloured swirls. This cake is topped with vanilla buttercream icing and rainbow sprinkles.', 17, 12, 7, 2),
-	('Red Velvet Cake', 'A soft, rectantular shaped red velvet cake topped with cream cheese icing and red velvet cake crumbs', 4, 5, 9, 3),
-	('Gluten-free Vanilla Cake', 'Round delicious, moist gluten-free vanilla cake topped with gluten-free vanilla buttercream icing and rasberries.', 8, 18, 10, 1),
-	('Angel Food Cake', 'Doughnut shaped, soft and airy angel cake topped with fluffy vanilla buttercream icing and various berries.', 18, 12, 11, 7),
-	('Classic Chiffon Cake', 'Round chiffon cake topped with vanilla buttercream icing and edible flower petals.', 19, 12, 12, 1),
-	('Orange Chiffon Cake', 'Round chiffon orange-flavoured cake topped with orange-flavoured buttercream icing and orange slices.', 19, 19, 13, 1),
-	('Orange Chocolate Cake', 'Round chocolate and orange flavoured cake topped with chocolate ganache icing and orange chocolate slices.', 20, 6, 14, 1),
-	('Classic Carrot Cake', 'Round carrot cake topped with cream cheese icing and chopped pecan nuts.', 5, 5, 15, 1),
-	('Christmas Cake', 'Round fruit cake topped with white fondant and cherries.', 7, 7, 1, 1)
+	('Red Velvet Cake', 'A soft, rectantular shaped red velvet cake topped with cream cheese icing and red velvet cake crumbs', 4, 5, 9, 2),
+	('Gluten-free Vanilla Cake', 'Round delicious, moist gluten-free vanilla cake topped with gluten-free vanilla buttercream icing and rasberries.', 8, 18, 10, 2),
+	('Angel Food Cake', 'Donut shaped, soft and airy angel cake topped with fluffy vanilla buttercream icing and various berries.', 18, 12, 11, 2),
+	('Classic Chiffon Cake', 'Round chiffon cake topped with vanilla buttercream icing and edible flower petals.', 19, 12, 12, 2),
+	('Orange Chiffon Cake', 'Round chiffon orange-flavoured cake topped with orange-flavoured buttercream icing and orange slices.', 19, 19, 13, 2),
+	('Orange Chocolate Cake', 'Round chocolate and orange flavoured cake topped with chocolate ganache icing and orange chocolate slices.', 20, 6, 14, 2),
+	('Classic Carrot Cake', 'Round carrot cake topped with cream cheese icing and chopped pecan nuts.', 5, 5, 15, 2),
+	('Christmas Cake', 'Round fruit cake topped with white fondant and cherries.', 7, 7, 1, 2)
 GO
