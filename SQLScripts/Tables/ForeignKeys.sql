787211885ccf9acb6b4a722920e2104e85451a11
MikaylaBBD <mikaylan@bbd.co.za>
2022-02-24 13:18:49 +0200

    formatted foreign key

--- SQLScripts/Tables/ForeignKeys.sql
+++ SQLScripts/Tables/ForeignKeys.sql
@@ -1,29 +1,18 @@
 USE [LetThemEatCakeDB]
 GO
 
-ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_IcingID] FOREIGN KEY (IcingID) REFERENCES Icing(IcingID);
-GO
-
-ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_ToppingID] FOREIGN KEY (ToppingID) REFERENCES Topping(ToppingID);
-GO
-
-ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_ShapeID] FOREIGN KEY (ShapeID) REFERENCES Shape(ShapeID);
-GO
-
-ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_CustomerID] FOREIGN KEY (InventedByCustomerID) REFERENCES Customer(CustomerID);
-GO
-
-ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [FK_DeliveryLocationID]             FOREIGN KEY([DeliveryLocationID])   REFERENCES [dbo].[Location] ([LocationID]);
-ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [FK_FavouriteCakeID]                FOREIGN KEY([FavouriteCakeID])      REFERENCES [dbo].[Cake] ([CakeID]);
-GO
-ALTER TABLE [dbo].[Order] ADD CONSTRAINT [fk_Order_CustomerID]                  FOREIGN KEY([CustomerID])           REFERENCES [dbo].[Customer] ([CustomerID]);
-ALTER TABLE [dbo].[Order] ADD CONSTRAINT [fk_Order_StatusID]                    FOREIGN KEY([StatusID])             REFERENCES [dbo].[Status] ([StatusID]);
-GO
-ALTER TABLE [dbo].[Cart] ADD CONSTRAINT [fk_Cart_OrderID]                       FOREIGN KEY([OrderID])              REFERENCES [dbo].[Order] ([OrderID]);
-ALTER TABLE [dbo].[Cart] ADD CONSTRAINT [fk_Cart_CakeID]                        FOREIGN KEY([CakeID])               REFERENCES [dbo].[Cake] ([CakeID]);
-GO
-ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_OrderID]               FOREIGN KEY([OrderID])              REFERENCES [dbo].[Order] ([OrderID]);
-ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_DeliveryLocationID]    FOREIGN KEY([DeliveryLocationID])   REFERENCES [dbo].[Customer] ([DeliveryLocationID]);
-ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_StatusID]              FOREIGN KEY([StatusID])             REFERENCES [dbo].[Status] ([StatusID]);
+ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_IcingID] FOREIGN KEY (IcingID) REFERENCES [dbo].[Icing] ([IcingID]);
+ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_ToppingID] FOREIGN KEY (ToppingID) REFERENCES [dbo].[Topping] (]ToppingID]);
+ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_ShapeID] FOREIGN KEY (ShapeID) REFERENCES [dbo].[Shape] (]ShapeID]);
+ALTER TABLE [dbo].[Cake] ADD CONSTRAINT [FK_CustomerID] FOREIGN KEY (InventedByCustomerID) REFERENCES [dbo].[Customer] ([CustomerID[]);
+ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [FK_DeliveryLocationID] FOREIGN KEY([DeliveryLocationID]) REFERENCES [dbo].[Location] ([LocationID]);
+ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [FK_FavouriteCakeID] FOREIGN KEY([FavouriteCakeID]) REFERENCES [dbo].[Cake] ([CakeID]);
+ALTER TABLE [dbo].[Order] ADD CONSTRAINT [fk_Order_CustomerID] FOREIGN KEY([CustomerID]) REFERENCES [dbo].[Customer] ([CustomerID]);
+ALTER TABLE [dbo].[Order] ADD CONSTRAINT [fk_Order_StatusID] FOREIGN KEY([StatusID]) REFERENCES [dbo].[Status] ([StatusID]);
+ALTER TABLE [dbo].[Cart] ADD CONSTRAINT [fk_Cart_OrderID] FOREIGN KEY([OrderID]) REFERENCES [dbo].[Order] ([OrderID]);
+ALTER TABLE [dbo].[Cart] ADD CONSTRAINT [fk_Cart_CakeID] FOREIGN KEY([CakeID]) REFERENCES [dbo].[Cake] ([CakeID]);
+ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_OrderID] FOREIGN KEY([OrderID]) REFERENCES [dbo].[Order] ([OrderID]);
+ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_DeliveryLocationID] FOREIGN KEY([DeliveryLocationID]) REFERENCES [dbo].[Customer] ([DeliveryLocationID]);
+ALTER TABLE [dbo].[Delivery] ADD CONSTRAINT [fk_Delivery_StatusID] FOREIGN KEY([StatusID]) REFERENCES [dbo].[Status] ([StatusID]);
 GO
 
