8f3491f22706ba586267b8520e0aa354f633bc2a
MikaylaBBD <mikaylan@bbd.co.za>
2022-02-24 11:56:15 +0200

    updated run scripts file

--- SQLScripts/RunScripts.sql
+++ SQLScripts/RunScripts.sql
@@ -1,3 +1,18 @@
-:setvar path "C:\Users\narom001\Documents\BBD GRAD\Level Ups\Databases\LetThemEatCake\SQLScripts"
+:setvar path "C:\Users\narom001\Documents\BBD GRAD\Level Ups\Databases\LetThemEatCake\SQLScripts" -- change to your own path
 :r $(path)CreateDatabase.sql
+:r $(path)\Tables\CreateCustomerTable.sql
+:r $(path)\Tables\CreateLocationTable.sql
+:r $(path)\Tables\CreateOrder.sql
+:r $(path)\Tables\CreateDelivery.sql
+:r $(path)\Tables\CreateCart.sql
+:r $(path)\Tables\CreateStatusTable.sql
 :r $(path)\Tables\CreateCakeTable.sql
+:r $(path)\Tables\CreateBase.sql
+:r $(path)\Tables\CreateIcing.sql
+:r $(path)\Tables\CreateTopping.sql
+:r $(path)\Tables\CreateShape.sql
+:r $(path)\AddConstraints.sql
+:r $(path)\Data\AddDummyData.sql
+:r $(path)\Stored Procedures\ToBeAdded.sql
+:r $(path)\Views\ToBeAdded.sql
+:r $(path)\User Defined Functions\ToBeAdded.sql
