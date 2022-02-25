24f109a707c9450a7b3b2dfce188f8e104244ebd
Wessel Scholtz <wessels@bbd.co.za>
2022-02-25 09:18:24 +0200

    Everthing works now, tested

--- SQLScripts/Stored Procedures/OrderPriceUpdate.sql
+++ SQLScripts/Stored Procedures/OrderPriceUpdate.sql
@@ -8,5 +8,11 @@ EXEC [dbo].[CalculateOrderPrice] @OrderID = 3
 EXEC [dbo].[CalculateOrderPrice] @OrderID = 4
 EXEC [dbo].[CalculateOrderPrice] @OrderID = 5
 EXEC [dbo].[CalculateOrderPrice] @OrderID = 6
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 7
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 8
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 9
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 10
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 11
+EXEC [dbo].[CalculateOrderPrice] @OrderID = 12
 
 GO
